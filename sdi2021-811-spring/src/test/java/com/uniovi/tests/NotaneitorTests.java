a47457185b8c2922382fe689af4e9954f953c5c3
raulng9 <salprez4@gmail.com>
2021-03-07 23:43:36 +0100

    SDI-2021-811-5.4-Casos Uso VistaHome.

--- sdi2021-811-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-811-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -7,53 +7,75 @@ import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorTests {
-	
+
 	static String PathFirefox65 = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
 	static String Geckdriver024 = "/Users/raul/selenium/geckodriver024mac";
 
 	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
 	static String URL = "http://localhost:8090";
-	
+
 	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
-		 System.setProperty("webdriver.firefox.bin", PathFirefox);
-		 System.setProperty("webdriver.gecko.driver", Geckdriver);
-		 WebDriver driver = new FirefoxDriver();
-		 return driver;
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
 	}
-	 
+
 	@Before
-	 public void setUp() throws Exception {
-		 driver.navigate().to(URL);
-	 }
-	 
-	
-	 @After
-	 public void tearDown() throws Exception {
-		 driver.manage().deleteAllCookies();
-	 }
-	 
-	 
-	 //Antes de la primera prueba
-	 @BeforeClass
-	 static public void begin() {
-		 
-	 }
-	 
-	 
-	 //Al finalizar la última prueba
-	 @AfterClass
-	 static public void end() {
-	 //Cerramos el navegador al finalizar las pruebas
-	     driver.quit();
-	 }
-	 
-	 
-	 @Test
-	 public void test() {
-		 fail("Not yet implemented");
-	 }
-	 
+	public void setUp() throws Exception {
+		driver.navigate().to(URL);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.manage().deleteAllCookies();
+	}
+
+	// Antes de la primera prueba
+	@BeforeClass
+	static public void begin() {
+
+	}
+
+	// Al finalizar la última prueba
+	@AfterClass
+	static public void end() {
+		// Cerramos el navegador al finalizar las pruebas
+		driver.quit();
+	}
+
+	// PR01. Acceder a la página principal /
+	@Test
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
 }
